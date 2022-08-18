bab17458d4eb38f2747b8864992e07df3948b5f5
Jett Thompson <jett.thompson.59@gmail.com>
2022-08-18 19:49:53 +1000

    add custom words

--- main.c
+++ main.c
@@ -1,109 +1,151 @@
 #include <limits.h>
+#include <stdint.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define STACK_SIZE 1024
 
-struct stack {
-  size_t top;
+struct context {
+  size_t stack_top;
   double stack[STACK_SIZE];
+  char *input_buffer;
+  size_t input_size;
+  size_t current_offset;
 };
 
-typedef void (*Handler)(struct stack *);
+typedef void (*Handler)(struct context *);
 
-#define PUSH(STACK, VALUE)                                                     \
+#define PUSH(CONTEXT, VALUE)                                                   \
   do {                                                                         \
-    if ((STACK)->top == STACK_SIZE) {                                          \
+    if ((CONTEXT)->stack_top == STACK_SIZE) {                                  \
       fprintf(stderr, "Stack overflow\n");                                     \
       exit(EXIT_FAILURE);                                                      \
     }                                                                          \
-    (STACK)->stack[(STACK)->top++] = (VALUE);                                  \
+    (CONTEXT)->stack[(CONTEXT)->stack_top++] = (VALUE);                        \
   } while (0)
-#define POP(STACK)                                                             \
-  ((STACK)->top > 0                                                            \
-       ? (STACK)->stack[--(STACK)->top]                                        \
+#define POP(CONTEXT)                                                           \
+  ((CONTEXT)->stack_top > 0                                                    \
+       ? (CONTEXT)->stack[--(CONTEXT)->stack_top]                              \
        : (fprintf(stderr, "Stack underflow\n"), exit(EXIT_FAILURE), 0))
 
-static void handle0(struct stack *stack) { PUSH(stack, 0); }
-static void handle1(struct stack *stack) { PUSH(stack, 1); }
-static void handle2(struct stack *stack) { PUSH(stack, 2); }
-static void handle3(struct stack *stack) { PUSH(stack, 3); }
-static void handle4(struct stack *stack) { PUSH(stack, 4); }
-static void handle5(struct stack *stack) { PUSH(stack, 5); }
-static void handle6(struct stack *stack) { PUSH(stack, 6); }
-static void handle7(struct stack *stack) { PUSH(stack, 7); }
-static void handle8(struct stack *stack) { PUSH(stack, 8); }
-static void handle9(struct stack *stack) { PUSH(stack, 9); }
-static void handle10(struct stack *stack) { PUSH(stack, 10); }
-static void handle11(struct stack *stack) { PUSH(stack, 11); }
-static void handle12(struct stack *stack) { PUSH(stack, 12); }
-static void handle13(struct stack *stack) { PUSH(stack, 13); }
-static void handle14(struct stack *stack) { PUSH(stack, 14); }
-static void handle15(struct stack *stack) { PUSH(stack, 15); }
-static void handlePlus(struct stack *stack) {
-  PUSH(stack, POP(stack) + POP(stack));
+static void handle0(struct context *context) { PUSH(context, 0); }
+static void handle1(struct context *context) { PUSH(context, 1); }
+static void handle2(struct context *context) { PUSH(context, 2); }
+static void handle3(struct context *context) { PUSH(context, 3); }
+static void handle4(struct context *context) { PUSH(context, 4); }
+static void handle5(struct context *context) { PUSH(context, 5); }
+static void handle6(struct context *context) { PUSH(context, 6); }
+static void handle7(struct context *context) { PUSH(context, 7); }
+static void handle8(struct context *context) { PUSH(context, 8); }
+static void handle9(struct context *context) { PUSH(context, 9); }
+static void handle10(struct context *context) { PUSH(context, 10); }
+static void handle11(struct context *context) { PUSH(context, 11); }
+static void handle12(struct context *context) { PUSH(context, 12); }
+static void handle13(struct context *context) { PUSH(context, 13); }
+static void handle14(struct context *context) { PUSH(context, 14); }
+static void handle15(struct context *context) { PUSH(context, 15); }
+static void handlePlus(struct context *context) {
+  PUSH(context, POP(context) + POP(context));
 }
-static void handleMinus(struct stack *stack) {
-  PUSH(stack, POP(stack) - POP(stack));
+static void handleMinus(struct context *context) {
+  PUSH(context, POP(context) - POP(context));
 }
-static void handleMultiply(struct stack *stack) {
-  PUSH(stack, POP(stack) * POP(stack));
+static void handleMultiply(struct context *context) {
+  PUSH(context, POP(context) * POP(context));
 }
-static void handleDivide(struct stack *stack) {
-  PUSH(stack, POP(stack) / POP(stack));
+static void handleDivide(struct context *context) {
+  PUSH(context, POP(context) / POP(context));
 }
-static void handleHexAppend(struct stack *stack) {
-  PUSH(stack, POP(stack) * 16 + POP(stack));
+static void handleHexAppend(struct context *context) {
+  PUSH(context, POP(context) * 16 + POP(context));
 }
-static void handleDuplicate(struct stack *stack) {
-  double value = POP(stack);
-  PUSH(stack, value);
-  PUSH(stack, value);
+static void handleDuplicate(struct context *context) {
+  double value = POP(context);
+  PUSH(context, value);
+  PUSH(context, value);
 }
-static void handlePrintChar(struct stack *stack) { putchar((int)POP(stack)); }
-static void handlePrintHexChar(struct stack *stack) {
-  handleHexAppend(stack);
-  handlePrintChar(stack);
+static void handlePrintChar(struct context *context) {
+  putchar((int)POP(context));
+}
+static void handlePrintHexChar(struct context *context) {
+  handleHexAppend(context);
+  handlePrintChar(context);
+}
+// Initialized to all -1.
+static size_t customWords[CHAR_MAX + 1];
+static void handleCustomWord(struct context *context) {
+  if (context->input_size > ++context->current_offset) {
+    char word = context->input_buffer[context->current_offset++];
+    customWords[word] = context->current_offset;
+    while (context->current_offset < context->input_size &&
+           context->input_buffer[context->current_offset++] != ';')
+      ;
+    if (context->current_offset == context->input_size) {
+      fprintf(stderr, "Unterminated custom word definition %c\n", word);
+      exit(EXIT_FAILURE);
+    }
+    context->current_offset--;
+  }
+}
+#define RETURN_STACK_SIZE 65536
+static size_t returnStack[RETURN_STACK_SIZE];
+static size_t returnStackTop = 0;
+static void handleReturn(struct context *context) {
+  if (returnStackTop > 0) {
+    context->current_offset = returnStack[--returnStackTop];
+  } else {
+    fprintf(stderr, "Return without matching call\n");
+    exit(EXIT_FAILURE);
+  }
 }
 
 static Handler handlers[CHAR_MAX + 1] = {
-    ['0'] = handle0,         ['1'] = handle1,
-    ['2'] = handle2,         ['3'] = handle3,
-    ['4'] = handle4,         ['5'] = handle5,
-    ['6'] = handle6,         ['7'] = handle7,
-    ['8'] = handle8,         ['9'] = handle9,
-    ['a'] = handle10,        ['b'] = handle11,
-    ['c'] = handle12,        ['d'] = handle13,
-    ['e'] = handle14,        ['f'] = handle15,
-    ['+'] = handlePlus,      ['-'] = handleMinus,
-    ['*'] = handleMultiply,  ['/'] = handleDivide,
-    ['~'] = handleHexAppend, [','] = handleDuplicate,
-    ['|'] = handlePrintChar, ['\\'] = handlePrintHexChar};
+    ['0'] = handle0,          ['1'] = handle1,
+    ['2'] = handle2,          ['3'] = handle3,
+    ['4'] = handle4,          ['5'] = handle5,
+    ['6'] = handle6,          ['7'] = handle7,
+    ['8'] = handle8,          ['9'] = handle9,
+    ['a'] = handle10,         ['b'] = handle11,
+    ['c'] = handle12,         ['d'] = handle13,
+    ['e'] = handle14,         ['f'] = handle15,
+    ['+'] = handlePlus,       ['-'] = handleMinus,
+    ['*'] = handleMultiply,   ['/'] = handleDivide,
+    ['~'] = handleHexAppend,  [','] = handleDuplicate,
+    ['|'] = handlePrintChar,  ['\\'] = handlePrintHexChar,
+    [':'] = handleCustomWord, [';'] = handleReturn};
 
 int main(int argc, char **argv) {
+  memset(customWords, 0xff, sizeof(customWords));
   if (argc < 2) {
     fprintf(stderr, "Missing file operand\n");
     return EXIT_FAILURE;
   }
-  FILE *inputStream = fopen(argv[1], "r");
-  fseek(inputStream, 0, SEEK_END);
-  size_t inputSize = ftell(inputStream);
-  rewind(inputStream);
-  char *input = malloc(inputSize * sizeof(char));
-  inputSize = fread(input, sizeof(char), inputSize, inputStream);
-  fclose(inputStream);
-  struct stack stack;
-  stack.top = 0;
-  for (size_t offset = 0; offset < inputSize; offset++) {
+  FILE *input_stream = fopen(argv[1], "r");
+  fseek(input_stream, 0, SEEK_END);
+  size_t input_size = ftell(input_stream);
+  rewind(input_stream);
+  char *input = malloc(input_size * sizeof(char));
+  input_size = fread(input, sizeof(char), input_size, input_stream);
+  fclose(input_stream);
+  struct context context;
+  context.stack_top = 0;
+  context.input_buffer = input;
+  for (size_t offset = 0; offset < input_size; offset++) {
+    context.current_offset = offset;
     char c = input[offset];
-    if (c <=CHAR_MAX && handlers[c]) {
-      handlers[c](&stack);
-    }else {
+    if (handlers[c]) {
+      handlers[c](&context);
+    } else if (customWords[c] != SIZE_MAX) {
+      returnStack[returnStackTop++] = offset;
+      context.current_offset = customWords[c] - 1;
+    } else {
       fprintf(stderr, "Unknown character %c\n", c);
       free(input);
       return EXIT_FAILURE;
     }
+    offset = context.current_offset;
   }
   free(input);
   return 0;
